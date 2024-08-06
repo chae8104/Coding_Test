d5fa8afc30ebe9fd9d75c65abcaff542174023b8
윤영채 <49390154+chae8104@users.noreply.github.com>
2024-08-06 20:15:53 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 984 ms, Memory: 2188 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int test_case;
+
+int main(){
+    cin >> test_case;
+    for(int i =0; i< test_case; i++){
+        int A,B;
+        cin >> A >> B;
+        vector<int> va(A);
+        vector<int> vb(B);
+        int answer = 0;
+        
+        for(int x = 0; x< A; x++){
+            cin >> va[x];
+        }
+        for(int y =0; y < B; y++){
+            cin >> vb[y];
+        }
+        sort(va.begin(), va.end());
+        sort(vb.begin(), vb.end());
+        
+        for(int p = 0; p < A; p++){
+            for(int q = 0; q < B; q++){
+                if(va[p] > vb[q]){
+                    answer++;
+                }
+            }
+        }
+        cout << answer << endl;
+        
+    }
+    return 0;
+}
