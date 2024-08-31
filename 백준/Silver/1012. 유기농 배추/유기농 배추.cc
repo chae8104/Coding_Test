bca20ce36054b8225ff9084379afc86d85319904
윤영채 <49390154+chae8104@users.noreply.github.com>
2024-08-31 09:10:17 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,55 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+int test_case, M, N, K;
 
-void dfs(int a, int b, vector<vector<int>> &map, vector<vector<int>> &visited) {
-    if (a < 0 || a >= map.size() || b < 0 || b >= map[0].size() || visited[a][b] || map[a][b] == 0) {
-        return;
-    }
-    
-    visited[a][b] = 1;
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
 
-    for (int direction = 0; direction < 4; direction++) {
-        int na = a + dx[direction];
-        int nb = b + dy[direction];
-        dfs(na, nb, map, visited);
+void dfs(int start_x, int start_y, vector<vector<int>> &v, vector<vector<int>> &visited){
+    visited[start_x][start_y] = 1;
+    for(int i = 0; i< 4; i++){
+        int nx = start_x + dx[i];
+        int ny = start_y + dy[i];
+        if(nx >=0 && ny >= 0 && nx < M && ny < N && v[nx][ny] == 1 && visited[nx][ny] != 1){
+            dfs(nx,ny,v,visited);
+        }
     }
 }
 
-int main() {
-    int test_case;
+int main(){
     cin >> test_case;
-
-    for (int test = 0; test < test_case; test++) {
-        int n, m, k;
-        cin >> n >> m >> k;
-
-        vector<vector<int>> map(n, vector<int>(m, 0));
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-
-        for (int cabbage = 0; cabbage < k; cabbage++) {
-            int x, y;
-            cin >> y >> x;
-            map[y][x] = 1; 
+    for(int i =0; i< test_case; i++){
+        cin >>M >> N >> K;
+        vector<vector<int>> v(M, vector<int> (N,0));
+        vector<vector<int>> visited(M, vector<int> (N,0));
+        for(int j =0; j< K; j++){
+            int a,b;
+            cin >> a >> b;
+            v[a][b] = 1;
         }
-
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    dfs(i, j, map, visited);
-                    count++;
+        for(int x = 0; x < M; x++){
+            for(int y = 0; y < N; y++){
+                if(v[x][y] == 1 && visited[x][y] != 1){
+                    dfs(x,y,v,visited);
+                    count ++;
                 }
             }
         }
-
         cout << count << endl;
     }
-
+    
     return 0;
 }
