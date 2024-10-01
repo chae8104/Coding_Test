db2c5707230bfc752f86707f512ad0cd61104b42
윤영채 <49390154+chae8104@users.noreply.github.com>
2024-10-01 22:08:12 +0900

    [Gold III] Title: 소수의 연속합, Time: 28 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;  
+bool che[4000001];
+int n, a[2000001], p, lo, hi, ret, sum; 
+ 
+int main(){
+    cin >> n;
+    for(int i=2; i<=n; i++){
+		if(che[i]) continue;
+		for(int j=2*i; j<=n; j+=i){
+			che[j] = 1;
+		}
+	}
+	for(int i=2; i<=n; i++){
+		if(!che[i]) a[p++] = i;
+	}  
+    while(true){
+        if(sum >= n) sum -= a[lo++];
+        else if(hi == p)break;
+        else sum += a[hi++];
+        if(sum == n) ret++;
+    }
+    cout << ret << endl;
+    
+    return 0;
+}
